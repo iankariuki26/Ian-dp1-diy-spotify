a4cad340917254b1637e5288edf9831b6fe3618a
Ian Kariuki <iankariuki@Ians-Macebook-Air.local>
2024-12-02 00:35:39 -0500

    this is it

--- fastapi-demo/fastapi-lab/app/fastapi_env/bin/activate.fish
+++ fastapi-demo/fastapi-lab/app/fastapi_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/iankariuki/UVADS/Systems - intro to computing/Project1/fastapi-lab/app/fastapi_env"
+set -gx VIRTUAL_ENV "/Users/iankariuki/UVADS/Systems - intro to computing/Project1/fastapi-demo/fastapi-lab/app/fastapi_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
