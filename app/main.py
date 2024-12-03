1a15054fbbaad830b1513a8dc480be5444ea6703
Ian Kariuki <iankariuki@Ians-Macebook-Air.local>
2024-12-03 01:20:38 -0500

    change

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ from fastapi import FastAPI
 
 DB = "epj7rf"
 
-DBHOST ='ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com'
-DBUSER ='admin'
+DBHOST =os.environ['DBHOST']
+DBUSER =os.environ['DBUSER']
 DBPASS = os.environ['DBPASS']
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur=db.cursor()
